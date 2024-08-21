891b2e59d00abff18951c825baea8b82908fc982
boyeon kim <hajoce02@naver.com>
2024-08-22 04:11:38 +0900

    third commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F76%2F3e%2F763e891be554e50ff47a5cf410d54ee877c1810f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/fullscreen]" alt="" />
+      <div>Conscious choice</div>
+      <div>벨티트 트윌 코트</div>
+      <div>99,000원</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ProductCard from "../component/ProductCard";
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-      전체 상품 페이지
+      <ProductCard />
     </div>
-  )
-}
+  );
+};
 
-export default ProductAll
+export default ProductAll;
