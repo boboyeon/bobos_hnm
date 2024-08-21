5f6c01d3c754acc2cc6a57f3772fd68d3c5f5a64
boyeon kim <hajoce02@naver.com>
2024-08-22 03:44:47 +0900

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
+
+// 1. 전체상품페이지, 로그인, 상품상세페이지
+// 1-1 네비게이션 바를 만든다 (모든페이지에서 고정)
+// 2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다.
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 4. 로그인이 되어있을 경우에는 상품 디테일 페이지를 볼 수 있다.
+// 5. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
+// 6. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
+// 7. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
+// 8. 상품을 검색할 수 있다.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+     <Routes>
+      <Route path='/' element={<ProductAll/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/product/:id' element={<ProductDetail/>}/>
+
+      </Routes> 
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Devided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+
+  return (
+    <div>
+      <div>
+        <div className="login_button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+
+      <div className="nav_section">
+        <img
+          width={100}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOSWOhkrtrLKgKz35SOCEsZV-v2q_yeKpMgw&s"
+          alt="메인로고"
+        />
+      </div>
+
+      <div className="menu_section">
+    <div className="menu_area">
+        <ul className="menu_list">
+            {menuList.map((menu, index) => (
+                <li key={index}>{menu}</li>
+            ))}
+        </ul>
+        <div className="input_area">
+            <FontAwesomeIcon className="icon_search" icon={faSearch} />
+            <input type="text" placeholder="Search..." />
+        </div>
+    </div>
+</div>
+    </div>
+  );
+};
+
+export default Navbar;
