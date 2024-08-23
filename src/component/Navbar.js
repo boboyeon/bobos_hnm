afcfa687bb7e1d42acc72b0a5cdbc7e04d3b6317
boyeon kim <hajoce02@naver.com>
2024-08-24 04:31:28 +0900

    15th commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -27,6 +27,10 @@ const Navbar = ({ isAuthenticated, setAuthenticate }) => {
     }
   };
 
+  const goToHome = () => {
+    navigate("/"); // 메인 화면으로 이동
+  };
+
   const search = (event) => {
     if (event.key === "Enter") {
       let keyword = event.target.value;
@@ -35,20 +39,24 @@ const Navbar = ({ isAuthenticated, setAuthenticate }) => {
   };
 
   return (
-    <BootstrapNavbar expand="lg" className="mb-3 custom-navbar">
+    <BootstrapNavbar expand="lg" className="custom-navbar">
       <Container fluid className="justify-content-between align-items-center">
-        <BootstrapNavbar.Toggle aria-controls="offcanvasNavbar" className="order-1" />
-        <BootstrapNavbar.Brand onClick={() => navigate("/")} className="order-2 mx-auto">
+        <BootstrapNavbar.Toggle aria-controls="offcanvasNavbar" className="order-lg-1" />
+
+        <BootstrapNavbar.Brand onClick={goToHome} className="mx-auto order-lg-2">
           <img
             width={100}
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOSWOhkrtrLKgKz35SOCEsZV-v2q_yeKpMgw&s"
             alt="메인로고"
+            style={{ cursor: "pointer" }}
           />
         </BootstrapNavbar.Brand>
-        <div className="login_button ms-3 order-3" onClick={goToLogin}>
+
+        <div className="login_button ms-3 order-lg-3" onClick={goToLogin}>
           <FontAwesomeIcon icon={faUser} />
           <div>{isAuthenticated ? "로그아웃" : "로그인"}</div>
         </div>
+
         <BootstrapNavbar.Offcanvas
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
@@ -58,13 +66,14 @@ const Navbar = ({ isAuthenticated, setAuthenticate }) => {
             <Offcanvas.Title id="offcanvasNavbarLabel">메뉴</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav className="justify-content-center flex-grow-1 pe-3 menu_list">
               {menuList.map((menu, index) => (
                 <Nav.Link key={index} onClick={() => navigate(`/${menu}`)}>
                   {menu}
                 </Nav.Link>
               ))}
             </Nav>
+
             <div className="d-flex align-items-center mt-3">
               <div className="input_area w-100">
                 <FontAwesomeIcon className="icon_search" icon={faSearch} />
